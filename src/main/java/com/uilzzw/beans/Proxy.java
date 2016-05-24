b008122427cea7d306a773206c134d3c2a76b718
uilzzw <uilzzw@gmail.com>
2016-05-24 22:41:57 +0800

    java bean

---
+++ src/main/java/com/uilzzw/beans/Proxy.java
@@ -0,0 +1,31 @@
+package com.uilzzw.beans;
+
+public class Proxy {
+	private String ipAddr;
+	private String port;
+	private String protocol;
+
+	public String getIpAddr() {
+		return ipAddr;
+	}
+
+	public void setIpAddr(String ipAddr) {
+		this.ipAddr = ipAddr;
+	}
+
+	public String getPort() {
+		return port;
+	}
+
+	public void setPort(String port) {
+		this.port = port;
+	}
+
+	public String getProtocol() {
+		return protocol;
+	}
+
+	public void setProtocol(String protocol) {
+		this.protocol = protocol;
+	}
+}
