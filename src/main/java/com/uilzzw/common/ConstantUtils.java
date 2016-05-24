d0e6ba3022e665ec01008c611286de40a0f76367
uilzzw <uilzzw@gmail.com>
2016-05-24 22:42:27 +0800

    ...

--- src/main/java/com/uilzzw/common/ConstantUtils.java
+++ src/main/java/com/uilzzw/common/ConstantUtils.java
@@ -4,9 +4,11 @@ import org.apache.log4j.Logger;
 
 public class ConstantUtils {
 	private static final Logger LOGGER = Logger.getLogger(ConstantUtils.class);
-
+	
 	public static Logger getLogger() {
 		return LOGGER;
 	}
-
+	public static String getUserHome(){
+		return System.getProperty("user.home");
+	}
 }
