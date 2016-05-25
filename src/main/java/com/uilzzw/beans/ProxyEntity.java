7115434fa6b27c62b45236f0a5e1167054e91436
uilzzw <uilzzw@gmail.com>
2016-05-25 14:35:51 +0800

    change proxy status

--- src/main/java/com/uilzzw/beans/ProxyEntity.java
+++ src/main/java/com/uilzzw/beans/ProxyEntity.java
@@ -1,41 +1,41 @@
 package com.uilzzw.beans;
 
 public class ProxyEntity {
-	private String ipAddress;
-	private String port;
-	private String protocol;
-	private boolean canBeUsed;
+    private String ipAddress;
+    private String port;
+    private String protocol;
+    // 0-Can used;1-Can't used;2-Don't Validate;3-Deleted;4-Died;
+    private int canBeUsed;
 
-	public String getIpAddress() {
-		return ipAddress;
-	}
+    public String getIpAddress() {
+	return ipAddress;
+    }
 
-	public void setIpAddress(String ipAddress) {
-		this.ipAddress = ipAddress;
-	}
+    public void setIpAddress(String ipAddress) {
+	this.ipAddress = ipAddress;
+    }
 
-	public String getPort() {
-		return port;
-	}
+    public String getPort() {
+	return port;
+    }
 
-	public void setPort(String port) {
-		this.port = port;
-	}
+    public void setPort(String port) {
+	this.port = port;
+    }
 
-	public String getProtocol() {
-		return protocol;
-	}
+    public String getProtocol() {
+	return protocol;
+    }
 
-	public void setProtocol(String protocol) {
-		this.protocol = protocol;
-	}
+    public void setProtocol(String protocol) {
+	this.protocol = protocol;
+    }
 
-	public boolean isCanBeUsed() {
-		return canBeUsed;
-	}
+    public int getCanBeUsed() {
+	return canBeUsed;
+    }
 
-	public void setCanBeUsed(boolean canBeUsed) {
-		this.canBeUsed = canBeUsed;
-	}
-
+    public void setCanBeUsed(int canBeUsed) {
+	this.canBeUsed = canBeUsed;
+    }
 }
